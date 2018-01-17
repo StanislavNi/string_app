53973be3e1d4d9d55cf8cd1b3d657809a8257b29
StanislavnNi <stanislavni1991@gmail.com>
2018-01-17 23:13:32 +0500

    changed the way of print and added docstring

--- string_app.py
+++ string_app.py
@@ -22,14 +22,30 @@ def max_string(s, n):
     return [text[i:i+n] for i in range(0, len(text), n)]
 
 for s in max_string(text, 25):
-    print(s + '\n')
+    print(s, '\n')
 
 # Reverse the text
 def reverse(text):
+    """
+    Reverse user's text
+    from the last symbol
+    to the first
+    """
     return text[::-1]
 reversed_text = reverse(text)
 
 # Print reversed strings with the length 25 symbols
 divided_reversed = ([reversed_text[i:i+25] for i in range(0, len(reversed_text), 25)])
 for a in divided_reversed:
-    print(a + '\n')
+    print(a, '\n')
+
+
+
+
+
+
+
+
+
+
+
