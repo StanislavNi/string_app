b6fb5b7d14dd3cf8ab6824793ba2c543326cb7ef
StanislavnNi <stanislavni1991@gmail.com>
2018-01-19 13:40:40 +0500

    added new classes

--- string_app.py
+++ string_app.py
@@ -1,14 +1,62 @@
+# coding: utf8
 # Script for working with the user's text
 
-class InputText(object):
-    def users_text(self):
+class UsersInput(object):
+    def get_text(self):
+        raise NotImplementedError
+
+    def is_valid(self, users_text):
+        raise NotImplementedError
+
+
+class FileText(UsersInput):
+    _filepath = None
+
+    def get_text(self):
+        f = open(self._filepath, 'r')
+        return f.read()
+
+    def is_valid(self, users_text):
+        if users_text == 'text.txt':
+            self._filepath = users_text
+            return True
+        return False
+
+
+class UrlText(UsersInput):
+
+    def get_text(self):
+
+        return 'Яндекс.Поиск'
+
+    def is_valid(self, users_text):
+        users_text = users_text.strip()
+        if users_text in ('https://ya.ru', 'ya.ru', 'http://ya.ru'):
+            return True
+        return False
+
+class InputText(UsersInput):
+    users_text = ''
+
+    def get_text(self):
         """
         Return input
         user's text
         """
-        return input("Введите ваш текст ")
+        return self.users_text
 
-text = InputText().users_text()
+    def is_valid(self, users_text):
+        self.users_text = users_text
+        return True
+
+
+user_raw_input = input('Введите команду, строку, путь к файлу или URL')
+#text = InputText().get_text()
+editor = UrlText()
+if editor.is_valid(user_raw_input):
+    text = editor.get_text()
+else:
+    raise Exception('Невозможно обработать ввод')
 
 # Count the length of the text
 print('Длина строки равна: ', len(text))
@@ -19,7 +67,7 @@ string_list = text.split()
 print('Количество слов в тексте равно: ', len(string_list))
 
 # Count the number of numerals
-num = ([int(i) for i in text if i.isdigit()])
+num = ([int(i) for i in string_list if i.isdigit()])
 print('Количество чисел в тексте равно: ', len(num))
 
 # Divide the text by strings with the length 25 symbols
@@ -28,12 +76,12 @@ def max_string(s, n):
     Divide 's'(user's text)
     by 'n'(string length)
     """
+    #TODO add description for user
     return [text[i:i+n] for i in range(0, len(text), n)]
 
 for s in max_string(text, 25):
     print(s, '\n')
 
-# Reverse the text
 def reverse(text):
     """
     Reverse user's text
@@ -44,9 +92,10 @@ def reverse(text):
 reversed_text = reverse(text)
 
 # Print reversed strings with the length 25 symbols
-divided_reversed = ([reversed_text[i:i+25] for i in range(0, len(reversed_text), 25)])
+divided_reversed = (
+    [reversed_text[i:i+25] for i in range(0, len(reversed_text), 25)])
 for a in divided_reversed:
-    print(a, '\n')
+    print(a)
 
 
 
